2923e927aafc6a7416a03b57e59908545992b6d5
robtplayground <rob.thwaites@playgroundxyz.com>
2017-02-19 22:58:20 +1100

    Updated file structure. TODO: relink files together.

---
+++ bin/scss.js
@@ -0,0 +1,48 @@
+"use strict";
+
+const sass = require('node-sass')
+const CleanCSS = require('clean-css');
+const autoprefixer = require('autoprefixer');
+const postcss      = require('postcss');
+const glob = require('glob');
+const path = require('path');
+const fs = require('fs');
+
+const files = glob.sync(path.join(__dirname, '/../scss/*.scss'));
+const outpath = path.join(__dirname, '/../css/');
+
+files.forEach((f) => {
+
+  const filename = f;
+  sass.render({
+    file: filename
+  }, function(err, result) {
+    if (err) {
+      console.warn(err);
+      process.exit(1);
+    }
+    const clean = new CleanCSS();
+    const basename = path.basename(filename, '.scss');
+    const css = clean.minify(result.css).styles;
+    const outFile = path.join(outpath, `${basename}.css`);
+
+
+
+    fs.writeFileSync(outFile, css.toString());
+    console.log(`css written to ${outFile}`);
+
+    // postcss([ autoprefixer ]).process(outFile).then(function (result) {
+    //   console.log('Autoprefixer running');
+    //     result.warnings().forEach(function (warn) {
+    //         console.warn(warn.toString());
+    //     });
+    //     fs.writeFileSync(outFile, result.toString());
+    //     console.log(result.css);
+    // });
+
+  });
+
+
+
+
+});
