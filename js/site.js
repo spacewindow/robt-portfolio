f86d1f7a247aee8cb5914abe00154d899b530b15
robtplayground <rob.thwaites@playgroundxyz.com>
2017-04-01 21:35:23 +1100

    Updates

---
+++ js/site.js
@@ -0,0 +1,8 @@
+$('body').scroll(function(){
+    console.log($('.body-scroll').scrollTop());
+  // $('.scroller').scrollLeft(scrollPos);
+});
+
+// $('.scroller').scroll(function(){
+//   console.log('scroller going left');
+// });
