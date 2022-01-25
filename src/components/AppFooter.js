5d27528dca502c1a30198c89b0a61a2d5e896611
Rob Thwaites <rob@robthwaites.com>
2022-01-25 18:20:02 +1100

    Updated Campaign Analytics video timing and captions

--- src/components/AppFooter.js
+++ src/components/AppFooter.js
@@ -4,15 +4,17 @@ import mq from "../style/mediaQueries";
 
 function AppFooter(props) {
   return (
-    <section class="footer-links">
+    <section className="footer-links">
       {props.previous && (
-        <a class="previous" href={`/work/${props.previous.id}`}>
-          <div class="footer-links__thumb">
-            <span class="footer-links__client">{props.previous.client}</span>
+        <a className="previous" href={`/work/${props.previous.id}`}>
+          <div className="footer-links__thumb">
+            <span className="footer-links__client">
+              {props.previous.client}
+            </span>
 
-            <span class="footer-links__title">{props.previous.title}</span>
+            <span className="footer-links__title">{props.previous.title}</span>
           </div>
-          <div class="footer-links__chevron footer-links__chevron--left">
+          <div className="footer-links__chevron footer-links__chevron--left">
             <svg viewBox="0 0 300 300" preserveAspectRatio="xMidYMid meet">
               <use x="0" y="0" xlinkHref="#chevron-left"></use>
             </svg>
@@ -20,12 +22,12 @@ function AppFooter(props) {
         </a>
       )}
       {props.next && (
-        <a class="next" href={`/work/${props.next.id}`}>
-          <div class="footer-links__thumb">
-            <span class="footer-links__client">{props.next.client}</span>
-            <span class="footer-links__title">{props.next.title}</span>
+        <a className="next" href={`/work/${props.next.id}`}>
+          <div className="footer-links__thumb">
+            <span className="footer-links__client">{props.next.client}</span>
+            <span className="footer-links__title">{props.next.title}</span>
           </div>
-          <div class="footer-links__chevron footer-links__chevron--right">
+          <div className="footer-links__chevron footer-links__chevron--right">
             <svg viewBox="0 0 300 300" preserveAspectRatio="xMidYMid meet">
               <use x="0" y="0" xlinkHref="#chevron-right"></use>
             </svg>
