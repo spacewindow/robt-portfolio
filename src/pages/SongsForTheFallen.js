68338d0e5be08d117e0df854d1a9f64597382065
Rob Thwaites <rob@robthwaites.com>
2022-01-28 17:31:01 +1100

    Songs page imported

--- src/pages/SongsForTheFallen.js
+++ src/pages/SongsForTheFallen.js
@@ -1,6 +1,13 @@
 import AppFooter from "../components/AppFooter";
 import ProjectHero from "../components/ProjectHero";
 import VideoSection from "../components/VideoSection";
+import Image1 from "../images/songs-for-the-fallen/songs-title.jpg";
+import Image2 from "../images/songs-for-the-fallen/songs-details1.jpg";
+import Image3 from "../images/songs-for-the-fallen/songs-details2.jpg";
+import Image4 from "../images/songs-for-the-fallen/songs-details3.jpg";
+import Image5 from "../images/songs-for-the-fallen/songs-details4.jpg";
+import Image6 from "../images/songs-for-the-fallen/songs-brand1.png";
+import Image7 from "../images/songs-for-the-fallen/songs-brand2.png";
 
 function SongsForTheFallen() {
   const projectId = "songs-for-the-fallen";
@@ -12,13 +19,7 @@ function SongsForTheFallen() {
         id={projectId}
         color="#c80511"
       />
-      <VideoSection
-        screenSize="desktop"
-        id={projectId}
-        videoFileName="video-songs-update.mp4"
-        chapterFileName="songs-chapters.vtt"
-        swiperHeading="Features"
-      />
+
       <section className="section--grey section--intro">
         <div className="grid">
           <div
@@ -37,6 +38,76 @@ function SongsForTheFallen() {
             <p>Front-End Developmemt</p>
             <p>Wordpress CMS</p>
           </div>
+        </div>
+      </section>
+      <VideoSection
+        screenSize="desktop"
+        id={projectId}
+        videoFileName="video-songs-update.mp4"
+        chapterFileName="songs-chapters.vtt"
+        swiperHeading="Features"
+      />
+      <section className="section--details first">
+        <div className="grid">
+          <div className="grid-cell grid6 intro-text">
+            <img src={Image2} alt="" />
+          </div>
+          <div className="grid-cell grid6 credits">
+            <img src={Image3} alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="section--details last">
+        <div className="grid">
+          <div className="grid-cell grid6 intro-text">
+            <img src={Image4} alt="" />
+          </div>
+          <div className="grid-cell grid6 credits">
+            <img src={Image5} alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section className="section--grey section--image-blocks">
+        <div className="image-block image-block--left">
+          <div className="grid">
+            <div className="grid-cell grid12">
+              <h4>Branding</h4>
+              <p>
+                I developed new visual identity for the show, which was
+                subsequently used across web, print and social media channels.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          className="image-block image-block--right"
+          style={{ backgroundImage: `url(${Image1})` }}
+        ></div>
+      </section>
+
+      <section className="section--details">
+        <div className="grid">
+          <div className="grid-cell grid6 intro-text">
+            <img src={Image6} alt="" />
+          </div>
+          <div className="grid-cell grid6 credits">
+            <img src={Image7} alt="" />
+          </div>
+        </div>
+      </section>
+
+      <section
+        className="section--grey section--image-full"
+        id="songs-responsive"
+      >
+        <div className="caption-block">
+          <h4>Responsive Design</h4>
+          <p>
+            Each component was designed to display perfectly on all device
+            sizes.
+          </p>
         </div>
       </section>
       <AppFooter
